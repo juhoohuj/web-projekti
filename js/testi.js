7655e5b6f3d37efe0ba0d7340f9370e1d862e029
Louhela <niko.louhela@gmail.com>
2021-12-01 10:54:45 +0200

    simppeli numeroiden arvonta

--- js/testi.js
+++ js/testi.js
@@ -1,16 +1,54 @@
 let progressPercent = 0
 let questionNumber = 1
 
+let first = random(1, 100)
+let second = random(1, 100)
+
+let correct = first + second
+
+
+function start(){
+
+        document.getElementById("question").textContent = first + " + " + second
+
+        document.getElementById("option1").textContent = correct
+        document.getElementById("option2").textContent = random(1,100)
+        document.getElementById("option3").textContent = random(1,100)
+        document.getElementById("option4").textContent = random(1,100)
+
+}
+
+function random(min, max){
+   return Math.floor(Math.random() * max) + min;
+}
+
 
 function test(){
+        first = random(1, 100)
+        second = random(1, 100)
+        correct = first + second
+       
+
         progressPercent += 20
         questionNumber += 1
         document.getElementById("progress").style.width = progressPercent + '%'
         document.getElementById("progress").textContent = progressPercent + '%'
         document.getElementById("questionNumber").textContent = questionNumber
+        document.getElementById("question").textContent = first + " + " + second
+
+
+        document.getElementById("option1").textContent = correct
+        document.getElementById("option2").textContent = random(1, 100)
+        document.getElementById("option3").textContent = random(1, 100)
+        document.getElementById("option4").textContent = random(1, 100)
+
+
+        
+
     if(questionNumber > 5){
         document.getElementById("questionNumber").textContent = "5"
         document.getElementById("progress").style.width = "100%"
+        document.getElementById("progress").textContent = "100%"
         document.getElementById("complete").style.display = "block"
     }
     

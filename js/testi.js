01696e73c7a9e26dcd3b5bada4022174232d919e
Louhela <niko.louhela@gmail.com>
2021-12-01 10:31:45 +0200

    mockup

--- js/testi.js
+++ js/testi.js
@@ -1,10 +1,19 @@
 let progressPercent = 0
+let questionNumber = 1
 
 
 function test(){
-    progressPercent += 10
-    document.getElementById("progress").style.width = progressPercent + '%'
-    document.getElementById("progress").textContent = progressPercent + '%'
+        progressPercent += 20
+        questionNumber += 1
+        document.getElementById("progress").style.width = progressPercent + '%'
+        document.getElementById("progress").textContent = progressPercent + '%'
+        document.getElementById("questionNumber").textContent = questionNumber
+    if(questionNumber > 5){
+        document.getElementById("questionNumber").textContent = "5"
+        document.getElementById("progress").style.width = "100%"
+        document.getElementById("complete").style.display = "block"
+    }
+    
 }
 
  

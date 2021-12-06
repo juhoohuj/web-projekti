db96b73eeb095896824fe433e973164ff123058d
Nino <ninorahikkala@gmail.com>
2021-12-06 16:49:41 +0200

    Etusivu_edistys

--- js/code.js
+++ js/code.js
@@ -1,1 +1,17 @@
-//web-projekti
+//Hieman Javascriptiä jotta diveistä saa muutettua linkit.
+
+document.getElementById('plus').addEventListener('click', function() {
+    location.href = 'pages/sivu_ahonen.html'
+}, false);
+
+document.getElementById('minus').addEventListener('click', function() {
+    location.href = 'pages/sivu_kujala.html'
+}, false);
+
+document.getElementById('jako').addEventListener('click', function() {
+    location.href = 'pages/sivu_louhela.html'
+}, false);
+
+document.getElementById('kerto').addEventListener('click', function() {
+    location.href = 'pages/sivu_rahikkala.html'
+}, false);

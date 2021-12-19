59c1b790b0835922c117a195a871ebfa39ebdd19
Louhela <niko.louhela@gmail.com>
2021-12-19 04:06:18 +0200

    kommenttien lisäys

--- js/louhela.js
+++ js/louhela.js
@@ -13,41 +13,43 @@ let option = [document.getElementById("option1"),
     document.getElementById("option3"),
     document.getElementById("option4")
 ]
-
+//Satunnaisen numeron arvonta
 function random(min, max) {
     return Math.floor(Math.random() * max) + min;
 }
-
-
+//Satunniasen numeron arvonta kahdella desimaalilla
 function randomWithDecimal() {
     let precision = 100;
     let randomnum = Math.floor(Math.random() * (10 * precision - 1 * precision) + 1 * precision) / (1 * precision);
     return randomnum
 }
-
+//Aloitus funkito kun sivu on ladattu
 function start() {
+    //Vastaustila asetetaan falseksi eli käyttäjä ei ole vastannut tämän hetkiseen kysymykseen. 
     answered = false;
     questionNumber = 1
     progressPercent = 0
     score = 0
-
+    progress.style.width = "0%"
+    progress.textContent = "0%"
+    //Tulos- ja alustapainikkeet pillotetaan.
     document.getElementById("result").style.display = "none"
     document.getElementById("restart").style.display = "none"
     document.getElementById("question").textContent = first + " / " + second
-
+    //Arvotaan vastauspainikkeisiin vastausvaihtoehdot
     option[0].textContent = randomWithDecimal()
     option[1].textContent = randomWithDecimal()
     option[2].textContent = randomWithDecimal()
     option[3].textContent = randomWithDecimal()
-
+    //Satunnaisesti valittuun vastauspainikkeeseen asetetaan oikean vastauksen arvo
     option[random(0, 4)].textContent = correct
 }
-
-function check(id) {
+//Vastauksen tarkistaminen
+function check(answerID) {
     if (answered == false) {
-        let answer = document.getElementById(id)
+        let answer = document.getElementById(answerID)
         document.getElementById("next").style.display = "inline"
-
+        //Jos vastaus on oikein asetetaan vastauspainikkeen luokaksi btn-success ja tulokseen lisätään piste jos vastaus väärin luokaksi btn-danger
         if (answer.textContent == correct) {
             answer.classList.toggle('btn-success');
             score++
@@ -57,12 +59,13 @@ function check(id) {
         answered = true;
     }
 }
-
+//Seuraava kysymys
 function next() {
+    //Vastaustila asetetaan falseksi.
     answered = false;
-
+    //Piilotetaan painike jolla siirrytään seuraavaan kysymykseen
     document.getElementById("next").style.display = "none"
-
+    //Vastauspainikkeista poistetaan tyylit
     option[0].classList.remove("btn-success")
     option[0].classList.remove("btn-danger")
 
@@ -74,36 +77,39 @@ function next() {
 
     option[3].classList.remove("btn-success")
     option[3].classList.remove("btn-danger")
-
+    //Uusi kysymys
     newQuestion()
 }
 
 
 function newQuestion() {
-    if (questionNumber > 4) {
+    //Jos käyttäjä on vastannut viiteen kysymykseen tuodaan esille painike jolla voidaan aloittaa tehtävät alusta ja sekä tuodaan esille oikein menneiden vastausen määrä
+    if (questionNumber == 5) {
         document.getElementById("restart").style.display = "inline"
         progress.style.width = "100%"
         progress.textContent = "100%"
         document.getElementById("result").style.display = "block"
         document.getElementById("result").textContent = "Tuloksesi: " + score
         answered = true;
+        //Arvotaan uudet numerot uuteen kysymykseen
     } else {
         first = random(1, 100)
         second = random(1, 100)
         correct = Math.round(first / second * 100) / 100
-
+        //edistymispalkki etenee 20% ja kysymysnumero kasvaa yhdellä
         progressPercent += 20
         questionNumber += 1
         progress.style.width = progressPercent + '%'
         progress.textContent = progressPercent + '%'
         document.getElementById("questionNumber").textContent = questionNumber
+        //Asetetaan arvotut numerot uuteen kysymyksen
         document.getElementById("question").textContent = first + " / " + second
-
+        //Vastauspainikkeisiin arvotaan vastausvaihtoehdot
         option[0].textContent = randomWithDecimal()
         option[1].textContent = randomWithDecimal()
         option[2].textContent = randomWithDecimal()
         option[3].textContent = randomWithDecimal()
-
+        //Satunnaisesti vailittuun vastauspainikkeeseen asetetaan arvoksi oikea vastaus
         option[random(0, 4)].textContent = correct
     }
 }

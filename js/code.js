54eccd6138bf328e4b5d30eeadb060cb3221c4ad
Louhela <niko.louhela@gmail.com>
2021-12-19 10:25:08 +0200

    responsiivisuutta etusivulle

--- js/code.js
+++ js/code.js
@@ -15,3 +15,19 @@ document.getElementById('jako').addEventListener('click', function() {
 document.getElementById('kerto').addEventListener('click', function() {
     location.href = 'pages/sivu_rahikkala.html'
 }, false);
+
+document.getElementById('mobilePlus').addEventListener('click', function() {
+    location.href = 'pages/sivu_ahonen.html'
+}, false);
+
+document.getElementById('mobileMinus').addEventListener('click', function() {
+    location.href = 'pages/sivu_kujala.html'
+}, false);
+
+document.getElementById('mobileJako').addEventListener('click', function() {
+    location.href = 'pages/sivu_louhela.html'
+}, false);
+
+document.getElementById('mobileKerto').addEventListener('click', function() {
+    location.href = 'pages/sivu_rahikkala.html'
+}, false);
